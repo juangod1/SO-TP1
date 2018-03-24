f250c8c2a88f73049b979789beafbeb1590c2c8e
pabloito <pradnic@itba.edu.ar>
2018-03-24 19:17:36 -0300

    First view commit

--- view.c
+++ view.c
@@ -1,8 +1,69 @@
 //
 // Created by juangod on 24/03/18.
 //
+#include "messaqueQueue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <mqueue.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <string.h>
+
+void testPrintToTerminal();
+void testPrintAfterChange();
+
+void givenAString();
+void givenAChange();
+
+void whenPrintingToStandardOutput();
+
+void thenSuccess();
+
+void setString(char * stringValue);
+
+char * parameter;
 
 int main(){
+    printf("Testing Print parameter to terminal...\n");
+    testPrintToTerminal();
 
+    printf("Testing Print after a change of value...\n");
+    testPrintAfterChange();
 }
 
+void testPrintAfterChange(){
+  givenAString();
+  givenAChange();
+
+  whenPrintingToStandardOutput();
+
+  thenSuccess();
+}
+
+void testPrintToTerminal(){
+  givenAString();
+  whenPrintingToStandardOutput();
+  thenSuccess();
+}
+
+void givenAString(){
+  setString("String");
+}
+
+void givenAChange(){
+  setString("Change");
+}
+
+void setString(char * stringValue){
+  parameter=stringValue;
+}
+
+void whenPrintingToStandardOutput(){
+  printf("%s\n",parameter);
+}
+
+void thenSuccess(){
+  printf("Ok!\n");
+}
