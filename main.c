1fa8ea74558b12ad500a47782f400ff02bfb17ba
pabloito <pradnic@itba.edu.ar>
2018-04-01 00:40:20 -0300

    Test Mode entrance

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "messageQueue.h"
 #include "master.h"
 
 int main(int argc, const char ** argv){
-    if(argc==1){ // Argument zero is the program name
+    if(argc==1) // Argument zero is the program name
+    {
         perror("Error: 0 program arguments received.\nExiting program...\n");
         exit(-1);
     }
-
-    //TODO: RECEIVE OPTION TEST and send a 1 or 0 to run, accordingly
-
-    run(argc,argv,0);
+    if(argc == 2 && strcmp(argv[1], "test")==0) //test Mode check
+    {
+      printf("Entering test mode...\n\t...\n\t\t...\n");
+      run(argc,argv,1);
+    }
+    else
+    {
+      run(argc,argv,0);
+    }
 }
